3b8f77842ad784f4b204de7ba88d255e2df23061
Atuldubey98 <atuldubey017@gmail.com>
2023-10-23 11:32:05 +0530

    fix : cors and features

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
+import cors from "cors";
 import express, { Application, Response } from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import { MONGO_URI, isEnvDev } from "./config";
-
 mongoose
   .connect(MONGO_URI)
   .then(() => {
@@ -12,11 +12,11 @@ mongoose
     console.error(err);
   });
 const app: Application = express();
-
+app.use(express.json());
+app.use(cors());
 app.use(morgan(isEnvDev ? "dev" : "combined"));
 
 app.get("/api", (_, res: Response) => {
   return res.send("Welcome to Express & TypeScript Server");
 });
-
 export default app;
