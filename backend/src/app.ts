eb316eebb68ae42027dfd0e8879a4ca7dfdc8958
Atul Dubey <atuldubey017@gmail.com>
2023-11-04 18:15:16 +0530

    feature : backend message

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,6 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan(isEnvDev ? "dev" : "combined"));
-
 app.get("/api", (_, res: Response) => {
   return res.send("Welcome to Express & TypeScript Server");
 });
